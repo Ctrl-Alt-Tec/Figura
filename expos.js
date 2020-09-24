6de522527b25991c2f17be99f554eef541e0a09f
Miguel Frias <22214@oaklawnacademy.org>
2020-09-24 00:35:12 -0500

    Expos añadido y actualizado
    Falta el URL para los posts

---
+++ expos.js
@@ -0,0 +1,28 @@
+
+
+/*
+async function fetchy(url){
+	let raw = await fetch(url);
+	let data = await raw.json();
+	return data;
+}
+
+const gallery = await fetchy('https://cors-anywhere.herokuapp.com/ctrl-alt-tec.herokuapp.com/Insert Fetch Url Here')
+
+FETCH URL NEEDED
+
+//properties  Data type    Desc.
+//artist	String[]	Nombre de artista(s)
+//artMedium	String[]	Material utilizado (óleo, acuarela, acrílico)
+//artForm	Enum	Tipo de obra de arte. Los tipos deberán definirse por FIgura
+//dimensions	[ float, float ]	Array con el tamaño en cm
+//abstract	Text	Descripción del trabajo
+//dateCreated	Date	Fecha de creación
+//dateModified	Date	(Opcional) Fecha de modificación
+//datePublished	Date	Fecha en que se subió a la plataforma
+//resource	String (url)	Dirección URL que contiene el recurso
+//feedback	Feedback[]	Array con de Objetos Feedback con calificación según criterios, comentarios, juez, etc.
+//tags	String[]	Array con las etiquetas
+
+*/
+
